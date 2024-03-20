eb57c46eed18c049ad846344be63c82eafeeb724
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-20 23:13:39 +0300

    Created Home Controller.

---
+++ HelloWebApi/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWebApi.Controllers
+{
+    //Api controller kodu api desteklemesi için yazılmıştır.
+    [ApiController]
+    //Yönlendirme mekanizmasını home adı altına adreslemek için yazılmıştır.
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+
+
+    }
+}
