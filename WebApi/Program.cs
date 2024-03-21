9fc24e64cf8201e87fae15d248f4fede61ccac6a
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-22 00:03:02 +0300

    Fixed solution problems.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<RepositoriesContext>( options => 
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("sqlConnection")
+        )
+);
 
 var app = builder.Build();
 
--- WebApi/Repositories/RepositoriesContext.cs
+++ WebApi/Repositories/RepositoriesContext.cs
@@ -1,6 +1,17 @@
-﻿namespace WebApi.Repositories
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Repositories
 {
-    public class RepositoriesContext
+    public class RepositoriesContext : DbContext
     {
+        public RepositoriesContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+
     }
 }
