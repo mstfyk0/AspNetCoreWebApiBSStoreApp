98d92d830a603ac508549006a0214220a7c23a9c
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-21 17:39:25 +0300

    Made Http Get, Post, Put, Delete, Patch examples.

--- BookDemo/Controllers/BooksController.cs
+++ BookDemo/Controllers/BooksController.cs
@@ -1,11 +1,141 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BookDemo.Data;
+using BookDemo.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookDemo.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/books")]
     [ApiController]
     public class BooksController : ControllerBase
     {
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books;
+            return Ok(books);
+
+        }
+
+        //GetOneBook methodu için id parametresi alınması gerekiyor şeklinde ayarladık.
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id" )] int id )
+        {
+            //Linq sorgusu ile parametreye gönderilen id ile books da ki id ye eşit olan kayıt getirilecek.
+            var book = ApplicationContext
+                .Books
+                .Where(b => b.Id == id)
+                .SingleOrDefault();
+
+            //Eğer book da veri yoksa NotFound yani bulunamadı hatası veriyoruz.
+            if (book  is null) 
+                return NotFound();
+
+
+            return Ok(book);
+
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody]Book book)
+        {
+            try
+            {
+
+                if (book is null)
+                    return BadRequest(); //400
+
+                ApplicationContext.Books.Add(book);
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message); //400
+                //throw;
+            }
+
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute (Name ="id")] int id  , [FromBody] Book book)
+        {
+            try
+            {
+                //check book
+                var entity = ApplicationContext
+                    .Books.Find(b => b.Id == id);
+
+                if (entity is null)
+                    return NotFound(); //404
+
+                //check id 
+                if (book.Id != id )
+                    return BadRequest(); //400
+
+                ApplicationContext.Books.Remove(entity);
+                book.Id = entity.Id;
+                ApplicationContext.Books.Add(book);
+
+                return Ok(book);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message); //400
+                //throw;
+            }
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                //check book
+                var entity = ApplicationContext
+                    .Books.Find(b => b.Id == id);
+
+                if (entity is null)
+                    return NotFound(); //404
+
+
+                ApplicationContext.Books.Remove(entity);
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message); //400
+                //throw;
+            }
+
+        }
+
+        [HttpPatch("{id:int}")]
+        public IActionResult PartialUpdateOneBook([FromRoute(Name = "id")] int id , [FromBody] JsonPatchDocument< Book> bookPatch)
+        {
+            try
+            {
+                //check book
+                var entity = ApplicationContext
+                    .Books.Find(b => b.Id == id);
+
+                if (entity is null)
+                    return NotFound(); //404
+
+
+                bookPatch.ApplyTo(entity);
+                return NoContent();    
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message); //400
+                //throw;
+            }
+
+        }
     }
 }
--- BookDemo/Data/ApplicationContext.cs
+++ BookDemo/Data/ApplicationContext.cs
@@ -1,6 +1,19 @@
-﻿namespace BookDemo.Data
+﻿using BookDemo.Models;
+
+namespace BookDemo.Data
 {
-    public class ApplicationContext
+    public static class ApplicationContext
     {
+
+        public static List<Book> Books { get; set; }
+        static ApplicationContext()
+        {
+            Books= new List<Book>()
+            {
+                new Book() { Id=1 , Title="Karagöz ve hacıvat" , Price=75 },
+                new Book() { Id=2 , Title="Mesnevi" , Price=150 },
+                new Book() { Id=3 , Title="Dede korkut" , Price= 100}
+            };
+        }
     }
 }
