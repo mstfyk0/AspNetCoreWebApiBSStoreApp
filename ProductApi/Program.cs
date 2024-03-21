858da3b9e0286f589863e4144c313cac7c1bf0d6
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-21 09:40:35 +0300

    Created two appsettingsjson (Development , Production). Edited to environment variable for Development and production. Edited file in program.cs, changed to work two environment variable in swagger.

--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -7,14 +7,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Logging yapýsý servis kayýtlarýnýn projeye eklenmesi.
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//iki ortam deðiþkeninde de çalýþmasý için if bloðunu yorum satýrý aldýk.
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
