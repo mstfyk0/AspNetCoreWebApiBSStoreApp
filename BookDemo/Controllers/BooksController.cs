44c5062eb033cbd2d4ecb082b19fbb9e863f1ef6
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-21 10:06:28 +0300

    Created Books controller. Added Id, Title and Price field in Book models.

---
+++ BookDemo/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
--- BookDemo/Models/Book.cs
+++ BookDemo/Models/Book.cs
@@ -2,6 +2,8 @@
 {
     public class Book
     {
-
+        public int Id { get; set; }
+        public String? Title { get; set; }
+        public decimal Price { get; set; }
     }
 }
