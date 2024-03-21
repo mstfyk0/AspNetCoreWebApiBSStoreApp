23d44d45e9868c4db2882e6eb135bbba00cb10a6
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-21 09:11:44 +0300

    Added to Dependecy Injection in ProductController class. Created a logger example.

--- ProductApi/Controllers/ProductsController.cs
+++ ProductApi/Controllers/ProductsController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProductApi.Models;
 
 namespace ProductApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/products")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ILogger<ProductsController> _logger;
 
+        //Dependecy Injection 
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {Id = 1, ProductName="Computer" },
+                new Product() {Id = 2, ProductName="Keyboard" },
+                new Product() {Id = 3, ProductName="Mouse" }
+            };
+            _logger.LogInformation(" GetAllProducts action has been called.");
+            return Ok(products);
+        }
     }
 }
