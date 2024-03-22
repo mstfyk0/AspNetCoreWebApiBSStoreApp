bf90248aa30761f27fb7fb1134b91b8f3cd4f8ab
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-22 08:59:17 +0300

    Created new Interface name IBookRepository. Added standard CRUD process in repositoryBase

--- Repositories/EFCore/RepositoryBase.cs
+++ Repositories/EFCore/RepositoryBase.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repositories.EFCore
 {
-    internal class RepositoryBase
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
     {
+
+        protected readonly RepositoriesContext _context;
+
+        public RepositoryBase(RepositoriesContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(T entity) => _context.Set<T>().Add(entity);  
+
+        public void Delete(T entity)=> _context.Set<T>().Remove(entity);
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ? _context.Set<T>().AsNoTracking()
+            : _context.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+            => !trackChanges ?
+            _context.Set<T>().Where(expression).AsNoTracking()
+            : _context.Set<T>().Where(expression);
+
+
+        public void Update(T entity) => _context.Set<T>().Update(entity);
     }
 }
