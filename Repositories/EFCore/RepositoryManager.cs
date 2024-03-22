9e58dd37d54300d39e481ef320eb7abe866129b2
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-22 11:05:21 +0300

    Created new file Extensions and in new class ServiceExtensions. Added dependency injection, Save method and Lazy loading IBookRepository and used to be value of loading in Repository Manager class

--- Repositories/EFCore/RepositoryManager.cs
+++ Repositories/EFCore/RepositoryManager.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Repositories.Contracts;
 
 namespace Repositories.EFCore
 {
-    internal class RepositoryManager
+    public class RepositoryManager : IRepositoryManager
     {
+
+        private readonly RepositoriesContext _repositoriesContext;
+        private readonly Lazy<IBookRepository> _bookRepository; 
+
+        public RepositoryManager(RepositoriesContext repositoriesContext)
+        {
+            _repositoriesContext = repositoriesContext;
+            _bookRepository = new Lazy<IBookRepository>(new BookRepository(_repositoriesContext));
+        }
+
+        public IBookRepository BookRepository => _bookRepository.Value;
+
+        public void Save()
+        {
+            _repositoriesContext.SaveChanges();
+        }
     }
 }
