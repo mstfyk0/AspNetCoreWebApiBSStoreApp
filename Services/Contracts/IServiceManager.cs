2bebbbf19db601c924f45ac4b0f99db270d05ab0
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-22 14:53:57 +0300

    Added IBookService in IserviceManager Create new class ServiceManager

--- Services/Contracts/IServiceManager.cs
+++ Services/Contracts/IServiceManager.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Services.Contracts
 {
-    internal interface IServiceManager
+    public interface IServiceManager
     {
+        IBookService BookService { get; }
     }
 }
