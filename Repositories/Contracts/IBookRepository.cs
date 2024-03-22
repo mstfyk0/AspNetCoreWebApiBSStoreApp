9790fe13abc5d80d062f8bbf5557b392b4674ecb
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-22 09:02:40 +0300

    Edited by IBookRepository. Create new class BookRepository.

--- Repositories/Contracts/IBookRepository.cs
+++ Repositories/Contracts/IBookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Repositories.Contracts
 {
-    internal interface IBookRepository
+    public interface IBookRepository : IRepositoryBase<Book>
     {
+
     }
 }
