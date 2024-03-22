945a4c543b811e9efd9fdc0ff4dbce3e03ffd589
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-22 10:49:32 +0300

    Create new class name RepositoryManager.  Updated interface IRepositoryManager

--- Repositories/Contracts/IRepositoryManager.cs
+++ Repositories/Contracts/IRepositoryManager.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Repositories.Contracts
 {
-    internal interface IRepositoryManager
+    public interface IRepositoryManager
     {
+        IBookRepository BookRepository { get; }
+        void Save();
     }
 }
