b7783e6e2ffb37bcd553718c808a97fe563d1f6b
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-23 11:18:26 +0300

    Added migration configuration. Create new file RepositoryFactory. Added new class RepositoryContextFactory

--- WebApi/RepositoryFactory/RepositoryContextFactory.cs
+++ WebApi/RepositoryFactory/RepositoryContextFactory.cs
@@ -1,6 +1,26 @@
-﻿namespace WebApi.RepositoryFactory
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repositories.EFCore;
+
+namespace WebApi.RepositoryFactory
 {
-    public class RepositoryContextFactory
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoriesContext>
     {
+        public RepositoriesContext CreateDbContext(string[] args)
+        {
+            //Configuration 
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            //DbContextOptionBuilder
+            var builder = new DbContextOptionsBuilder<RepositoriesContext>()
+                .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+                prj => prj.MigrationsAssembly("WebApi"));
+
+            return new RepositoriesContext(builder.Options);
+
+        }
     }
 }
