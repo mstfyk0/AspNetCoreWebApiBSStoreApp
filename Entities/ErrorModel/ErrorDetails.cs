b0e3f4d8301a663ed89ce2992ac9b70c33e49240
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-23 12:26:07 +0300

    Edited class to ErrorDetails Created new class in file Extensions

--- Entities/ErrorModel/ErrorDetails.cs
+++ Entities/ErrorModel/ErrorDetails.cs
@@ -1,8 +1,17 @@
 ﻿
 
+using System.Text.Json;
+
 namespace Entities.ErrorModel
 {
-    internal class ErrorDetails
+    public class ErrorDetails
     {
+        public int StatusCode  { get; set; }
+        public string? Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
     }
 }
---
+++ WebApi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApi.Extensions
+{
+    public class ExceptionMiddlewareExtensions
+    {
+
+    }
+}
