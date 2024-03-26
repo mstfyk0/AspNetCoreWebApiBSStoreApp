1b9499e7fba0a6ae5b444fe394e9765a41e38ebf
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-03-26 12:11:47 +0300

    updated

--- Entities/Models/ErrorModel/ErrorDetails.cs
+++ Entities/Models/ErrorModel/ErrorDetails.cs
@@ -2,11 +2,11 @@
 
 using System.Text.Json;
 
-namespace Entities.ErrorModel
+namespace Entities.Models.ErrorModel
 {
     public class ErrorDetails
     {
-        public int StatusCode  { get; set; }
+        public int StatusCode { get; set; }
         public string? Message { get; set; }
 
         public override string ToString()
