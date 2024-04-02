ea99a43cc8745611ecd4b9c7d7bbd62f13f029e8
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-04-02 10:22:18 +0300

    Added interface IAuthenticationService in services. Added UserRegisterDto object and added automapper profile.

--- Entities/DataTransferObjects/UserForRegistrationDto.cs
+++ Entities/DataTransferObjects/UserForRegistrationDto.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities.DataTransferObjects
 {
-    internal class UserForRegistrationDto
+    public record UserForRegistrationDto
     {
+        public String? FirstName { get; init; }
+        public String? Lastname { get; init; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public String? UserName { get; init; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public String? Password { get; init; }
+        public String? Email { get; init; }
+        public String? PhoneNumber { get; init; }
+        public ICollection<string>? Roles{ get; init; }
+
+
     }
 }
