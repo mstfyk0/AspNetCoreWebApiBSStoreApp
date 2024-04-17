774356a9af7022e64eebaf1902aef6a6eb3502ef
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-04-17 17:29:59 +0300

    continue implementation category entity

--- Services/Contracts/ICategoryService.cs
+++ Services/Contracts/ICategoryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Services.Contracts
 {
-    internal interface ICategoryService
+    public interface ICategoryService
     {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync(bool trackChanges);
+        Task<Category> GetOneCategoryByIdAsync(int id, bool trackChanges);
+        void CreateOneCategory(Category category);
+        void UpdateOneCategory(Category category);
+        void DeleteOneCategory(Category category);
     }
 }
