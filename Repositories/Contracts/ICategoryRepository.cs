074c3d0b225c2e987dd5daeec3fe2b63b420b28e
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-04-17 16:18:27 +0300

    Added category repository. Edited interface category repository.

--- Repositories/Contracts/ICategoryRepository.cs
+++ Repositories/Contracts/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Repositories.Contracts
 {
-    internal interface ICategoryRepository
+    public interface ICategoryRepository  : IRepositoryBase<Category>
     {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync( bool trackChanges);
+
     }
 }
